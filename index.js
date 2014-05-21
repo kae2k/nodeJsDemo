96b83b039293173ff7b6e0f313a25fed8d951bc3
kae2k <kae2k@virginia.edu>
2014-05-21 11:30:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var server = require("./server");
 var router = require("./route");
 var handlers = require("./handlers");
 var handle = {};
+handle[''] = handlers.onLoad;
 handle['subjectExpand'] = handlers.subjectExpand;
 handle['classExpand'] = handlers.classExpand;
 handle['sectionExpand'] = handlers.sectionExpand;
